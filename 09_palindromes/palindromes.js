7cc7de3dd8f52a89bc8c5c1e8b1204dc3f4ba2ce
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 18:49:34 -0500

    palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+    // Your code here
+    processedStr = str.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return(
+        processedStr 
+        .split("")
+        .reverse()
+        .join("") == processedStr
+    );
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// correct answer 01/02/2023
