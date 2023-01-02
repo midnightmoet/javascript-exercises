1bbc1f5de73f1a821a6bbc3b1948a51ab19f6760
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 17:00:04 -0500

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(minimum, maximum) {
+    // Your code here
+    if(!Number.isInteger(minimum) || !Number.isInteger(maximum)) return "ERROR";
 
+    if(minimum < 0 || maximum < 0) return "ERROR";
+
+    if(minimum > maximum){
+        const temp = minimum;
+        minimum = maximum;
+        maximum = temporary;
+    }
+
+    let sum = 0;
+    for(let i = minimum; i < maximum +1; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
