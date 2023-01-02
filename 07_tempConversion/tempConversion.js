b70425f0483afaae02cc3f48d723a8339e4bc981
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 18:09:38 -0500

    tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = (f) => {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (c) => {
+  return Math.round(((c * 9/5) +  32) * 10) /10;
 };
 
 // Do not edit below this line
@@ -9,3 +11,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+// correct answer 01/02/2023
