a528df76df65c1c59eabfb1cbc04abe41f2ba68d
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 19:48:45 -0500

    snakeCase solution

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,6 +1,22 @@
-const snakeCase = function() {
+const snakeCase = (str) => {
+    // wtf case
+    str = str.replace(/\.\.\./g, ' ');
 
+    // camel case
+    if(str.indexOf(" ") < 0) {
+        str = str.replace(/([A-Z])/g, ' $1');
+    }
+
+    return str
+        .trim()
+        .toLowerCase()
+        .split(' ')
+        .join('_')
+        .replace(/[,\?\.]/g, '')
+        .replace(/\-/g, " ");
 };
 
 // Do not edit below this line
 module.exports = snakeCase;
+
+//  correct solution 01/02/2023
