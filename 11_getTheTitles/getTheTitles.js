9676a89d56dd746ab575abd6a79427f8838bee06
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 19:12:12 -0500

    getTheTitles completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = () => {
+  // Your code here
+    return array.map(book => book.title);
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// correct answer 01/02/2023
