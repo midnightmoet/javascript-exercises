7e8adb045c8990c09b536659c6f25af9a3dca90a
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 19:33:54 -0500

    findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = (array) => {
+    // Your code here
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
+
+const getAge = (birth, death) => {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// correct answer 01/02/2023
