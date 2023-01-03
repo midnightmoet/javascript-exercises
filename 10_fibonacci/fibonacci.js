ddd9226068c100bb3c381a99b4be7403fa8f3b21
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 19:08:58 -0500

    fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = (count) => {
+    // Your code here
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
 
+    let a = 0;
+    let b = 1;
+    for(let i = 1; i < count; i++){
+        const temp = b;
+        b = a+b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// correct answer 01/02/2023
