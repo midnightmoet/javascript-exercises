5f4a663c2d3d93eb3c527a3a24d143d4c6e05925
Lisa Miner <midnightmoet@gmail.com>
2023-01-02 19:37:36 -0500

    caesar solution

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,33 @@
-const caesar = function() {
-
+const caesar = (string, shift) => {
+    // Your code here
+    return string
+        .split('')
+        .map(char => shiftChar(char, shift))
+        .join('');
 };
 
+
+const codeSet = code => (code < 97 ? 65 : 97);
+
+
+// this function is just a fancy way of doing % so that it works with negative numbers
+// see this link for details:
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+
+const mod = (n, m) => ((n % m) + m) % m;
+
+const shiftChar = (char, shift) => {
+    const code = char.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        return String.fromCharCode(
+            mod(code + shift - codeSet(code), 26) + codeSet(code)
+        );
+    }
+    return char;
+}
 // Do not edit below this line
 module.exports = caesar;
+
+
+// correct answer 01/02/2023; had to look at solution
